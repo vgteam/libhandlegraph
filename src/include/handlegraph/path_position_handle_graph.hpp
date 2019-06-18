7c0e89a2c8c1edd31b7dad375f06e29f97c4d1e4
Erik Garrison <erik.garrison@gmail.com>
2019-06-18 14:30:00 +0200

    add path position handle graph

---
+++ src/include/handlegraph/path_position_handle_graph.hpp
@@ -0,0 +1,43 @@
+#ifndef HANDLEGRAPH_PATH_POSITION_HANDLE_GRAPH_HPP_INCLUDED
+#define HANDLEGRAPH_PATH_POSITION_HANDLE_GRAPH_HPP_INCLUDED
+
+/** \file 
+ * Defines the PathPositionHandleGraph interface for graphs that can have paths altered.
+ */
+
+#include "handlegraph/path_handle_graph.hpp"
+
+namespace handlegraph {
+
+/**
+ * This is the interface for a path handle graph with path positions
+ */
+class PathPositionHandleGraph : virtual public PathHandleGraph {
+public:
+
+    /// Execute a function on each step (step_handle_t) and its path relative position and orientation
+    /// on a handle in any path. If it returns bool and returns false, stop iteration.
+    /// Returns true if we finished and false if we stopped early.
+    template<typename Iteratee>
+    bool for_each_step_and_position_on_handle(const handle_t& handle, const Iteratee& iteratee) const;
+    
+protected:
+    
+    /// Execute a function on each step (step_handle_t) and its path relative position and orientation
+    /// on a handle in any path. If it returns bool and returns false, stop iteration.
+    /// Returns true if we finished and false if we stopped early.
+    virtual bool for_each_step_and_position_on_handle(
+        const handle_t& handle,
+        const std::function<bool(const step_handle_t&, const bool&, const uint64_t&)>& iteratee) const = 0;
+
+};
+
+template<typename Iteratee>
+bool PathHandleGraph::for_each_step_and_position_on_handle(const handle_t& handle, const Iteratee& iteratee) const {
+    return for_each_step_and_position_on_handle_impl(handle, BoolReturningWrapper<Iteratee, step_handle_t>::wrap(iteratee));
+}
+
+}
+
+#endif
+
