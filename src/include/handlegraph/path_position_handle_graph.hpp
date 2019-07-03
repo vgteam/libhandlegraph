bca572e3b9d7d008efd9013792a727575006fc36
Jordan Eizenga <joeizeng@gmail.com>
2019-07-03 08:07:55 -0700

    make the position iteration actually overridable

--- src/include/handlegraph/path_position_handle_graph.hpp
+++ src/include/handlegraph/path_position_handle_graph.hpp
@@ -15,6 +15,10 @@ namespace handlegraph {
  */
 class PathPositionHandleGraph : virtual public PathHandleGraph {
 public:
+    
+    ////////////////////////////////////////////////////////////////////////////
+    // Interface that needs to be implemented
+    ////////////////////////////////////////////////////////////////////////////
 
     /// Returns the length of a path measured in bases of sequence.
     virtual size_t get_path_length(const path_handle_t& path_handle) const = 0;
@@ -30,6 +34,10 @@ public:
     virtual step_handle_t get_step_at_position(const path_handle_t& path,
                                                const size_t& position) const = 0;
     
+    ////////////////////////////////////////////////////////////////////////////
+    // Additional optional interface with a default implementation
+    ////////////////////////////////////////////////////////////////////////////
+    
     /// Execute an iteratee on each step on a path, along with its orientation relative to
     /// the path (true if it is reverse the orientation of the handle on the path), and its
     /// position measured in bases of sequence along the path. Positions are always measured
@@ -37,8 +45,8 @@ public:
     ///
     /// Iteration will stop early if the iteratee returns false. This method returns false if
     /// iteration was stopped early, else true
-    bool for_each_step_position_on_handle(const handle_t& handle,
-                                          const std::function<bool(const step_handle_t&, const bool&, const size_t&)>& iteratee) const;
+    virtual bool for_each_step_position_on_handle(const handle_t& handle,
+                                                  const std::function<bool(const step_handle_t&, const bool&, const size_t&)>& iteratee) const;
     
     
 };
