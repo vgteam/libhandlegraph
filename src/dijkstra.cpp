5c24e28f3fe3784bafe0bb1acadef79acfc1937e
Xian Chang <xhchang@plaza.gi.ucsc.edu>
2022-07-15 17:57:50 -0700

    Fix bugs

--- src/dijkstra.cpp
+++ src/dijkstra.cpp
@@ -121,10 +121,14 @@ bool dijkstra(const HandleGraph* g, const unordered_set<handle_t>& starts,
 }
 
 void for_each_handle_in_shortest_path(const HandleGraph* g, handle_t start, handle_t end, std::function<bool(const handle_t&, size_t)> iteratee) {
+#ifdef debug_vg_algorithms
+    cerr << "Get shortest path from " << g->get_id(start) << (g->get_is_reverse(start) ? "rev" : "fd") 
+         << " to " << g->get_id(end) << (g->get_is_reverse(end) ? "rev" : "fd") << endl;
+#endif
     // Run dijktra copy and pasted from above, but record the traceback for each node 
 
     //Map each handle_t to the handle_t that came before it in the shortest path from start to it and the distance
-    unordered_map<handle_t, pair<handle_t, distance>> traceback_map;
+    unordered_map<handle_t, pair<handle_t, size_t>> traceback_map;
 
     // We keep a priority queue so we can visit the handle with the shortest
     // distance next. We put handles in here whenever we see them with shorter
@@ -151,6 +155,7 @@ void for_each_handle_in_shortest_path(const HandleGraph* g, handle_t start, hand
     // We keep a current handle
     handle_t current;
     size_t distance = 0;
+    handle_t backtrace;
     queue.push(make_tuple(distance, start, start));
     
     while (!queue.empty()) {
@@ -163,32 +168,32 @@ void for_each_handle_in_shortest_path(const HandleGraph* g, handle_t start, hand
         else {
             dequeued.insert(current);
         }
+        //Add this node and its predecessor to the traceback
+        traceback_map.emplace(current, make_pair(backtrace, distance));
+
         if (current == end) {
             //If we find the end node, stop
-            //clear the queue just in case break doesn't work
-            queue.clear();
             break;
         }
         
 #ifdef debug_vg_algorithms
         cerr << "Visit " << g->get_id(current) << " " << g->get_is_reverse(current) << " at distance " << distance << endl;
+        cerr << "\t adding to backtrace " << g->get_id(backtrace) << " " << g->get_is_reverse(backtrace) << endl;
 #endif    
 
 
-        //Add this node and its predecessor to the traceback
-        traceback_map.emplace(current, make_pair(backtrace, distance));
 
         // Remember that we made it here.
         visited.emplace(current);
         
-        if (!starts.count(current)) {
+        if (start != current) {
             // Up the distance with the node's length. We don't do this for the
             // start handles because we want to count distance from the *end* of
             // the start handles unless directed otherwise.
             distance += g->get_length(current);
         }
             
-        g->follow_edges(current, traverse_leftward, [&](const handle_t& next) {
+        g->follow_edges(current, false, [&](const handle_t& next) {
             // For each handle to the right of here
             
             if (!visited.count(next)) {
@@ -209,10 +214,14 @@ void for_each_handle_in_shortest_path(const HandleGraph* g, handle_t start, hand
         });
     }
 
+#ifdef debug_vg_algorithms
+cerr << "Traceback to start " << g->get_id(start) << (g->get_is_reverse(start) ? "rev" : "fd")<< endl;
+unordered_set<handle_t> seen;
+#endif
     //Now traceback maps each handle_t to the distance from start to it, and the thing that came before it in the 
     //shortest path
     //trace back from end to start, keeping a vector of the handles and distances ordered backwards
-    vector<pair<handle_t, distance>> backwards_traceback;
+    vector<pair<handle_t, size_t>> backwards_traceback;
     current = end;
     while (current != start) {
 
@@ -222,25 +231,36 @@ void for_each_handle_in_shortest_path(const HandleGraph* g, handle_t start, hand
         //Add current to the list
         backwards_traceback.emplace_back(current, predecessor.second);
 
+#ifdef debug_vg_algorithms
+cerr << "At " << g->get_id(current) << (g->get_is_reverse(current) ? "rev" : "fd") << " back to " 
+     <<  g->get_id(predecessor.first) << (g->get_is_reverse(predecessor.first) ? "rev" : "fd") << endl;
+if(seen.count(current)){
+    throw runtime_error("Already seen this ");
+}
+seen.emplace(current);
+#endif
+
         //And move back one step
         current = predecessor.first; 
     }
+#ifdef debug_vg_algorithms
+    cerr << "Go forwards to get shortest path" << endl;
+#endif
 
-    //Go backwards through backwards_traceback and call iteratee on everything
-    //backwards_traceback won't include start so do that with distance 0 too
+    //Go backwards through backwards_traceback and call iteratee on everything, excluding end
     if (backwards_traceback.size() == 0) {
-        iteratee(start, 0);
         return;
     }
-    for (int i = backwards_traceback.size()-1 ; i >= 0 ; i--) {
+    for (int i = backwards_traceback.size()-1 ; i >= 1 ; i--) {
         auto& x = backwards_traceback[i];
+#ifdef debug_vg_algorithms
+cerr << "At " << g->get_id(x.first) << (g->get_is_reverse(x.first) ? "rev" : "fd") << endl; 
+#endif
         //run iteratee and stop if it returned false
         if (!iteratee(x.first, x.second)) {
             return;
         }
     }
-    iteratee(start, 0);
-
 }
 
     
